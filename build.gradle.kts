9b05d3d361df67e30c731dd81575cac9a1526d02
saunshchikov <SAUnshchikov@nota.tech>
2025-11-08 15:23:51 +0300

    Migrate to Gradle version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
 	checkstyle
 	java
 	jacoco
-	id("org.springframework.boot") version "3.4.0"
-	id("io.spring.dependency-management") version "1.1.6"
+	alias(libs.plugins.spring.boot)
+	alias(libs.plugins.spring.dependency.management)
 }
 
 group = "ru.job4j.devops"
@@ -36,13 +36,13 @@ repositories {
 }
 
 dependencies {
-	compileOnly("org.projectlombok:lombok:1.18.36")
-	annotationProcessor("org.projectlombok:lombok:1.18.36")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-	testImplementation("org.assertj:assertj-core:3.24.2")
+	compileOnly(libs.lombok)
+	annotationProcessor(libs.lombok)
+	implementation(libs.spring.boot.starter.web)
+	testImplementation(libs.spring.boot.starter.test)
+	testRuntimeOnly(libs.junit.platform.launcher)
+	testImplementation(libs.junit.jupiter)
+	testImplementation(libs.assertj.core)
 }
 
 tasks.withType<Test> {
